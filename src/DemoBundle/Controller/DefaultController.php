63f2e1ccaa51b4d49744bea747c14c1b75dcc7da
julien <julien@novelcom.fr>
2017-11-02 21:13:47 +0100

    demo

--- src/DemoBundle/Controller/DefaultController.php
+++ src/DemoBundle/Controller/DefaultController.php
@@ -4,16 +4,35 @@ namespace DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations\Get;
-
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class DefaultController extends Controller
 {
     /**
-    @Get("/demo/test")
+     * DOC 2
+     *
+    @Get("/interco/test")
      */
     public function getIntercoTestAction()
     {
         die('test ok');
+    }
+
+    /**
+     *
+     * This is the documentation description of your method, it will appear
+     * on a specific pane. It will read all the text until the first
+     * annotation.
+     *
+     * @ApiDoc(
+     *  section="DOC_3",
+     *  description="API interface doc",
+     * )
+     * @Get("/interco/doc")
+     */
+    public function getIntercoApiDocAction()
+    {
+        die('doc ok');
     }
 
 }
